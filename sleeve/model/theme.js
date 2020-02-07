29392cc44e40dfc06dd44069b8df6d293afefb28
propoet <propoet@163.com>
2020-02-08 00:13:07 +0800

    ''

--- sleeve/model/theme.js
+++ sleeve/model/theme.js
@@ -1,8 +1,9 @@
 import {config} from "../config/config";
+import {Http} from "../utils/http";
 
 class Theme {
     static getHomeLocationA(callback){
-        wx.request({
+       /* wx.request({
             url: `${config.apiBaseUrl}theme/by/names`,
             method: 'GET',
             data: {
@@ -14,7 +15,18 @@ class Theme {
             success: (res) => {
                 callback(res.data)
             }
-        })
+        })*/
+
+       Http.request({
+           url:'theme/by/names',
+           data:{
+               names: 't-1'
+           },
+           callback:data=>{
+               callback(data)
+           }
+
+       })
     }
 }
 export {
--- sleeve/utils/http.js
+++ sleeve/utils/http.js
@@ -1,5 +1,22 @@
+import {config} from "../config/config";
+
 class Http {
-    static request(){
-        
+    static request({url,data,callback,method='GET'}){
+        //url data method
+        wx.request({
+            url:`${config.apiBaseUrl}${url}`,
+            data,
+            method,
+            header: {
+                appkey: config.appkey
+            },
+            success(res) {
+                callback(res.data)
+            }
+        })
     }
+}
+
+export {
+    Http
 }
