dee4f8e95fa87f30bb042f1a7aa0c48bfc7b3597
propoet <propoet@163.com>
2020-03-03 18:00:52 +0800

    java

---
+++ mall/src/test/java/com/imooc/mall/dao/CategoryMapperTest.java
@@ -0,0 +1,26 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.MallApplicationTests;
+import com.imooc.mall.pojo.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class CategoryMapperTest extends MallApplicationTests {
+  @Autowired
+  private CategoryMapper categoryMapper;
+/*  @Test
+  public void findById() {
+    Category category = categoryMapper.findById(100001);
+    System.out.println(category.toString());
+  }
+
+  @Test
+  public void queryById() {
+    Category category = categoryMapper.queryById(100001);
+    System.out.println(category.toString());
+  }*/
+}
